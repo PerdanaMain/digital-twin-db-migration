c2cb55038138fe052305244f6c1c9c3ad76b170d
MrWaradana <muhammadridhowaradana@gmail.com>
2024-08-20 11:13:57 +0700

    update: abc models

--- digital_twin_migration/models/abc.py
+++ digital_twin_migration/models/abc.py
@@ -1,6 +1,7 @@
 """
 Define an Abstract Base Class (ABC) for models
 """
+
 from datetime import datetime
 from weakref import WeakValueDictionary
 
@@ -11,8 +12,8 @@ from . import db
 
 
 class MetaBaseModel(db.Model.__class__):
-    """ Define a metaclass for the BaseModel
-        Implement `__getitem__` for managing aliases """
+    """Define a metaclass for the BaseModel
+    Implement `__getitem__` for managing aliases"""
 
     def __init__(cls, *args):
         super().__init__(*args)
@@ -28,15 +29,15 @@ class MetaBaseModel(db.Model.__class__):
 
 
 class BaseModel:
-    """ Generalize __init__, __repr__ and to_json
-        Based on the models columns """
+    """Generalize __init__, __repr__ and to_json
+    Based on the models columns"""
 
     print_filter = ()
     to_json_filter = ()
 
     def __repr__(self):
-        """ Define a base way to print models
-            Columns inside `print_filter` are excluded """
+        """Define a base way to print models
+        Columns inside `print_filter` are excluded"""
         return "%s(%s)" % (
             self.__class__.__name__,
             {
@@ -48,22 +49,22 @@ class BaseModel:
 
     @property
     def json(self):
-        """ Define a base way to jsonify models
-            Columns inside `to_json_filter` are excluded """
+        """Define a base way to jsonify models
+        Columns inside `to_json_filter` are excluded"""
         return {
-            column: value
-            if not isinstance(value, datetime)
-            else value.strftime("%Y-%m-%d")
+            column: (
+                value if not isinstance(value, datetime) else value.strftime("%Y-%m-%d")
+            )
             for column, value in self._to_dict().items()
             if column not in self.to_json_filter
         }
 
     def _to_dict(self):
-        """ This would more or less be the same as a `to_json`
-            But putting it in a "private" function
-            Allows to_json to be overriden without impacting __repr__
-            Or the other way around
-            And to add filter lists """
+        """This would more or less be the same as a `to_json`
+        But putting it in a "private" function
+        Allows to_json to be overriden without impacting __repr__
+        Or the other way around
+        And to add filter lists"""
         return {
             column.key: getattr(self, column.key)
             for column in inspect(self.__class__).attrs
@@ -80,3 +81,11 @@ class BaseModel:
 
     def commit(self):
         db.session.commit()
+
+    def add(self):
+        db.session.add(self)
+        return self
+
+    def rollback(self):
+        db.session.rollback()
+        
