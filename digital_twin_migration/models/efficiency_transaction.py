b7d517288a31c162219c058ed2fee708f2292431
Cizz22 <cisatraa@gmail.com>
2024-08-22 11:10:34 +0700

    fix model

--- digital_twin_migration/models/efficiency_transaction.py
+++ digital_twin_migration/models/efficiency_transaction.py
@@ -23,7 +23,7 @@ class EfficiencyTransaction(db.Model, BaseModel, metaclass=MetaBaseModel):
     created_by =  db.Column(UUID(as_uuid=True), nullable=False)
     updated_by =  db.Column(UUID(as_uuid=True), nullable=False)
     
-    details = db.relationship("EfficiencyTransactionDetail", backref="efficiency_transaction")
+    efficiency_transaction_details = db.relationship("EfficiencyTransactionDetail", backref="efficiency_transaction")
 
     
     def __init__(self, periode, jenis_parameter, excel_id, variable_id, nilai, created_by):
