dbaede5d7403bde64a163b86f948cdd177543e24
Perdana Main <firman.fp123@gmail.com>
2024-10-02 13:21:19 +0700

    migrate to PFI App

--- digital_twin_migration/models/authentication/__init__.py
+++
@@ -1,4 +0,0 @@
-from .resource import Resource
-from .role import Role
-from .user import User
-from .role_has_resource import RoleHasResource
--- digital_twin_migration/seeds/role.py
+++
@@ -1,8 +0,0 @@
-from digital_twin_migration.models.authentication import Role
-
-def roleSeeder():
-    positions = ["Admin", "User"]
-
-    for c in positions:
-        role = Role(name=c)
-        role.save()
